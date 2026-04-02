2a35aa2bc795f9be2dd343dfd14afc2cc54306b3
Joshua Junga <104138393+JostSenpai@users.noreply.github.com>
2026-04-02 17:12:19 +0200

    fixed python version

--- launcher.bat
+++ launcher.bat
@@ -2,8 +2,8 @@
 cd /d "%~dp0"
 
 echo [SYSTEM] Firing up the Autodarts Pipeline in the background...
-start /B python dartstats.py
+start /B py -3.12 dartstats.py
 
 echo [SYSTEM] Launching the Analytics Dashboard...
 :: Bypassing the Windows PATH issue by running Streamlit directly through Python!
-python -m streamlit run dashboard.py
+py -3.12 -m streamlit run dashboard.py
